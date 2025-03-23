1909faf5694914d38d68221606ed5ccb1215a1b0
madelynnn4 <146007401+madelynnn4@users.noreply.github.com>
2025-03-23 23:15:55 +0700

    implementasi design by contract

--- SayaTubeVideo.cs
+++ SayaTubeVideo.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 
 namespace SayaTubeVideoApp
 {
@@ -10,6 +11,9 @@ namespace SayaTubeVideoApp
 
         public SayaTubeVideo(string title)
         {
+            Debug.Assert(title != null, "Judul video tidak boleh null.");
+            Debug.Assert(title.Length <= 100, "Judul video maksimal 100 karakter.");
+
             Random random = new Random();
             this.id = random.Next(10000, 99999);
             this.title = title;
@@ -18,19 +22,42 @@ namespace SayaTubeVideoApp
 
         public void IncreasePlayCount(int count)
         {
-            this.playCount += count;
+            Debug.Assert(count <= 10000000, "Penambahan play count maksimal 10.000.000.");
+            Debug.Assert(count > 0, "Penambahan play count harus lebih dari 0.");
+
+            try
+            {
+                checked
+                {
+                    if (this.playCount > int.MaxValue - count)
+                    {
+                        throw new OverflowException();
+                    }
+                    this.playCount += count;
+                }
+            }
+            catch (OverflowException)
+            {
+                Console.WriteLine("Play count maksimum telah tercapai. Tidak bisa menambah lagi.");
+            }
         }
 
         public void PrintVideoDetails()
         {
-            Console.WriteLine("Video Title: " + this.title);
-            Console.WriteLine("Play Count: " + this.playCount);
+            Console.WriteLine("ID Video     : " + this.id);
+            Console.WriteLine("Judul Video  : " + this.title);
+            Console.WriteLine("Play Count   : " + this.playCount);
         }
 
         static void Main(string[] args)
         {
             SayaTubeVideo video1 = new SayaTubeVideo("Tutorial Design By Contract - Valentino Favian Bimo Khristora");
-            video1.IncreasePlayCount(5);
+
+            for (int i = 0; i < 250; i++)
+            {
+                video1.IncreasePlayCount(10000000);
+            }
+
             video1.PrintVideoDetails();
         }
     }
